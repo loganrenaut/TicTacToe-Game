f0affe2049ade12c238cd707c96d148301b59542
Logan Renaut <arenaut@asu.edu>
2026-06-08 22:25:43 -0700

    Add GameBoard and Player classes
    - GameBoard manages 3x3 grid state
    
    - Player class handles player turns
    
    - Basic input validation included
    
    - Foundation for game logic complete

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,8 @@
 public class Player {
 public static void main(String[] args) {
-System.out.println("Welcome, players!");
-//TODO Implement player class
-}
+        System.out.println("Welcome, players!");
+        //TODO Implement player class
+        // Demonstration change for git diff
+    // One more change for git diff
+    }
 }
